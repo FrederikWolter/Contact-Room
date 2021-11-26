577a9deda98cdd17dd354dad5c7c943fbcd7bad3
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2021-11-26 14:49:03 +0100

    Added getUser to UserDao.kt

--- app/src/main/java/com/example/roomapp/data/UserDao.kt
+++ app/src/main/java/com/example/roomapp/data/UserDao.kt
@@ -4,6 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.roomapp.model.User
 
+import android.database.sqlite.SQLiteDatabase
+
+import android.provider.ContactsContract.CommonDataKinds.Note
+import androidx.room.RoomMasterTable.TABLE_NAME
+
+
 @Dao
 interface UserDao {
 
@@ -21,5 +27,8 @@ interface UserDao {
 
     @Query("SELECT * FROM user_table ORDER BY id ASC")
     fun readAllData(): LiveData<List<User>>
+
+    @Query("SELECT * FROM user_table WHERE id = :id")
+    fun getUser(id: Int): User
 
 }
